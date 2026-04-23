8c386479f155180947640c92d1f2cfb45321d530
Sarah F. Alvi <sarah.f3458@gmail.com>
2026-04-24 04:16:02 +0500

    added categories of locations and colors

--- Point.h
+++ Point.h
@@ -2,7 +2,7 @@
 
 struct Point {
     float x, y;
-    int id; // optional identifier
+    int id; // optional identifier 0:schools, 1:hospitals, 2:restaurants, 3: office, 4: hotels, 5: universities
 
     Point() : x(0), y(0), id(-1) {}
     Point(float x, float y, int id = -1) : x(x), y(y), id(id) {}
